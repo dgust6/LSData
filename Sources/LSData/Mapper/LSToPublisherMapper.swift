5ba5ab1097b5fc6a2bfd3847f7ca4c8966ce62f8
Dino Gustin <dino.gustin@gmail.com>
2022-03-25 18:44:07 +0100

    Added missing public initializers

--- Sources/LSData/Mapper/LSToPublisherMapper.swift
+++ Sources/LSData/Mapper/LSToPublisherMapper.swift
@@ -9,6 +9,8 @@ public class LSToPublisherMapper<Input>: Mapper {
     public typealias Input = Input
     public typealias Output = AnyPublisher<Input, Never>
     
+    public init() {}
+    
     public func map(_ input: Input) -> Output {
         Just(input).eraseToAnyPublisher()
     }
